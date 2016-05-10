5e4881afe0602445033e382ace719e8daed1de9d
Ben Chatelain <ben@octop.ad>
2016-05-09 22:04:13 -0600

    Add SwiftKeywords enum

---
+++ SearchController/SwiftKeywords.swift
@@ -0,0 +1,66 @@
+//
+//  SwiftKeywords.swift
+//  SearchController
+//
+//  Created by Ben Chatelain on 5/9/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+enum SwiftKeywords: String {
+    case `associativity`
+    case `break`
+    case `case`
+    case `catch`
+    case `class`
+    case `continue`
+    case `convenience`
+    case `default`
+    case `deinit`
+    case `didSet`
+    case `do`
+    case `else`
+    case `enum`
+    case `extension`
+    case `fallthrough`
+    case `false`
+    case `final`
+    case `for`
+    case `func`
+    case `get`
+    case `guard`
+    case `if`
+    case `in`
+    case `infix`
+    //case `init`
+    case `inout`
+    case `internal`
+    case `lazy`
+    case `let`
+    case `mutating`
+    case `nil`
+    case `operator`
+    case `override`
+    case `postfix`
+    case `precedence`
+    case `prefix`
+    case `private`
+    case `public`
+    case `repeat`
+    case `required`
+    case `return`
+    case `self`
+    case `set`
+    case `static`
+    case `struct`
+    case `subscript`
+    case `super`
+    case `switch`
+    case `throws`
+    case `true`
+    case `try`
+    case `var`
+    case `weak`
+    case `where`
+    case `while`
+    case `willSet`
+}
